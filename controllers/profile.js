679c8d7cc3abc1857686249e2d87913742483f7d
lowyidah <lowyidah@gmail.com>
2021-08-04 22:44:17 +0800

    update

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,6 +2,10 @@
 const handleProfile = (req, res) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header('Access-Control-Allow-Credentials', true);
+
+    console.log("req.session:", req.session)
+        console.log("req.session.user1:", req.session.user)
+
     if(req.session.user) {
       res.json(req.session.user);
     }
